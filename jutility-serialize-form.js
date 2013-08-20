2a4a1717a25dc60ad4134d8faff38e7c3c87c8c0
Alex Goida <alex.goida@xtrmstep.ru>
2013-08-20 16:58:43 +0400

    Initial files

---
+++ jutility-serialize-form.js
@@ -0,0 +1,21 @@
+﻿// serializes FORM object to JSON representation
+// usage:
+//      var frm = $('#myForm');
+//      var obj = frm.serializeObject();
+(function($) {
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
---
+++ jutility-server-find-dialog.js
@@ -0,0 +1,17 @@
+﻿(function ($) {
+    $.fn.serverFindDialog = function (parameters) {
+        var options = {
+            urlDataRequestAction: '',
+            buttonRemoveCaption: '',
+            buttonAcceptCaption: '',
+            buttonCancelCaption: '',
+            // override standard element style
+            styles: {
+
+            },
+            selectedItemTemplate: '<span class="item" title="{{title}}"><button class="btn btn-mini btn-info" type="button" name="{{form-field}}" ><span class="item-text">{{title}}</span><i class="icon-remove" title="{{button-remove-caption}}"/></button></span>'
+        };
+        $.extend(options, parameters);
+    };
+
+})(jQuery);
