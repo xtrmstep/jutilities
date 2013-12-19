3b06a714497f379cdcfe6c77643c2bc8e7afc7e2
Alex Goida <alex.goida@xtrmstep.ru>
2013-12-19 15:35:08 +0400

    find dialog

--- jutility-server-find-dialog.js
+++ jutility-server-find-dialog.js
@@ -1,17 +1,38 @@
-﻿(function ($) {
-    $.fn.serverFindDialog = function (parameters) {
-        var options = {
-            urlDataRequestAction: '',
-            buttonRemoveCaption: '',
-            buttonAcceptCaption: '',
-            buttonCancelCaption: '',
-            // override standard element style
-            styles: {
+﻿/// <reference path="_references.js" />
 
-            },
-            selectedItemTemplate: '<span class="item" title="{{title}}"><button class="btn btn-mini btn-info" type="button" name="{{form-field}}" ><span class="item-text">{{title}}</span><i class="icon-remove" title="{{button-remove-caption}}"/></button></span>'
+(function ($) {
+    $.fn.serverDialog = function (parameters) {
+        var options = {
+            getCheckedItems: function ($dialog) { return null; },
+            getItemValue: function ($item) { return $item.val(); },
+            getItemName: function ($item) { return $item.data('name'); },
+            templatePrefix: '',
+            templateField: '',
+            saveButtonSelector: '.save',
+            selectedItemsContainer: ''
         };
         $.extend(options, parameters);
-    };
+        var selectedItemTemplate = '<span class="outer {2}"><input type="hidden" name="{3}" value="{0}"/><span>{1}<i class="icon-remove" onclick="$(\'.{2}\').remove();" ></i></span></span>';
 
+        var dialogWidget = this;
+        var $dialog = $(this);
+        var contentSelector = $dialog.selector + 'Content';
+
+        var saveButtonSelectorAbsolute = $dialog.selector + ' ' + options.saveButtonSelector;
+        $(document).on("click", saveButtonSelectorAbsolute, function () {
+            var items = options.getCheckedItems($dialog);
+            var itemsList = '';
+            for (var index = 0; index < items.length; index++) {
+                var $item = $(items[index]);
+                var value = options.getItemValue($item);
+                var text = options.getItemName($item);
+                var itemId = options.templatePrefix + "Span" + value;
+                itemsList += selectedItemTemplate.format(value, text, itemId, options.templateField);
+            }
+            var $itemsListHtml = $.parseHTML(itemsList);
+            $(options.selectedItemsContainer).children().remove().end().append($itemsListHtml);
+            $dialog.modal('hide');
+            $(contentSelector).children().remove();
+        });
+    };
 })(jQuery);
